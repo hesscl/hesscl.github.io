b8510c2f6f2eb000c64898951adad2bee371f3ed
hesscl <11861694+hesscl@users.noreply.github.com>
2026-03-01 20:03:56 +0000

    Deploying to gh-pages from @ hesscl/hesscl.github.io@79b94555c7a410482a855e11687f38d39c4fea36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,7 +59,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-quackrfss",
           title: 'quackrfss',
-          description: "Fast access to BRFSS survey data (1984–2024) via DuckDB",
+          description: "Fast access to BRFSS survey data (1990–2024) via DuckDB",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-coefinla",
